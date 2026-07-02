8f98385d69d88a245946d98ec51dd3c79b74bcdf
Berenger Beyogbin <bberengeryao@gmail.com>
2026-07-02 12:27:46 +0000

    chore: stabiliser la connexion PostgreSQL locale

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
     DB_PORT = os.getenv("DB_PORT", "5432")
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
